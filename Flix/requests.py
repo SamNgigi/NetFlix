15648a3303f30535d1f3a8bac7c282e82e87c79c
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-01 11:46:54 +0300

    Added Trailer

--- Flix/requests.py
+++ Flix/requests.py
@@ -1,2 +1,3 @@
 import requests
 from .models import Movie
+# Api key AIzaSyA52gIKTp4rE_-VBwwsWHb-uc1VZXJRTSY
--- Flix/views.py
+++ Flix/views.py
@@ -7,11 +7,17 @@ import requests
 def index(request):
     test = 'Working'
     response = requests.get(
-        'https://api.themoviedb.org/3/movie/550?api_key=5c680a4136f5ea00d164efb422804b99')
+        'http://api.themoviedb.org/3/movie/157336?api_key=5c680a4136f5ea00d164efb422804b99&append_to_response=videos')
     movie_data = response.json()
+    videos = movie_data['videos']
+    trailer = videos['results']
+    youtube_trailer = trailer[0]['key']
+    print(youtube_trailer)
+
     content = {
         "test": test,
         'title': movie_data['title'],
         'poster': movie_data['poster_path'],
+        'trailer': youtube_trailer
     }
     return render(request, 'index.html', content)
